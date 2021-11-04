5e79bebbe3aca3e51b61ec968b554372f792ea66
Fred <fgaurat@eolem.com>
2021-11-04 17:01:45 +0100

    thread

--- tp03_1/main.py
+++ tp03_1/main.py
@@ -7,7 +7,13 @@ def main():
     soup = BeautifulSoup(response.text, 'html.parser')
     all_a = [a['href'] for a in soup.find_all('a') if a['href'][0]!="?"]
 
-    for a in all_a:
-        print(a)
+    for log_file in all_a:
+        url_file = f"{url}/{log_file}"
+        resp = requests.get(url_file)
+        with open(log_file,'w') as f:
+            f.write(resp.text)
+
+
+
 if __name__ == '__main__':
     main()
